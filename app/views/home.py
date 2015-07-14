dd6280064831dcea5497aef4d971513c027c5ab6
Richard Bramley <rabramley@gmail.com>
2015-07-14 11:30:03 +0100

    Add counts to home screen

--- app/views/home.py
+++ app/views/home.py
@@ -2,8 +2,16 @@ from flask import g, render_template
 from flask.ext.login import login_required
 
 from app import telomere
-from app.model.user import User
+
+from app.model.batch import Batch
+from app.model.sample import Sample
+from app.model.measurement import Measurement
 
 @telomere.route('/')
 def index():
-    return render_template('index.html')
+	batches = Batch.query.count()
+	samples = Sample.query.count()
+	measurements = Measurement.query.count()
+
+	return render_template('index.html', batches=batches, samples=samples, measurements=measurements)
+
