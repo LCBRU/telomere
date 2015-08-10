37d2abc5f6057534780649ad4ccfa6e4814baed8
Richard Bramley <rabramley@gmail.com>
2015-08-10 15:08:51 +0100

    Forgot to take field out of model.

--- app/model/spreadsheet.py
+++ app/model/spreadsheet.py
@@ -3,7 +3,6 @@ from app import db
 class Spreadsheet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(500))
-    filepath = db.Column(db.String(1000))
     uploaded = db.Column(db.DateTime())
     processed = db.Column(db.DateTime())
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -12,4 +11,5 @@ class Spreadsheet(db.Model):
         self.id = kwargs.get('id')
         self.filename = kwargs.get('filename')
         self.uploaded = kwargs.get('uploaded')
+        self.processed = kwargs.get('processed')
         self.userId = kwargs.get('userId')
