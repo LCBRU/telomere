77662755b72ee6b6c188e2a346cb41bb777141f7
Richard Bramley <rabramley@gmail.com>
2015-11-05 16:09:42 +0000

    Remove manual batch entry

--- app/forms/batch.py
+++ app/forms/batch.py
@@ -1,32 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField, HiddenField, FieldList, FormField, DecimalField, Form as WtfForm
-from wtforms.validators import DataRequired, ValidationError, StopValidation, Length
-from wtforms_components import TimeField, read_only
+from wtforms import StringField, HiddenField, FormField, DecimalField, Form as WtfForm
+from wtforms.validators import DataRequired, Length
 from wtforms.fields.html5 import DateTimeField
-
-class RequiredIfSampleCode(object):
-    def __call__(self, form, field):
-    	if (not form.sampleCode.data):
-	        field.errors[:] = []
-	        # Stop further validators running
-	        raise StopValidation()
-
-class RequiredIfSampleDataEntered(object):
-    def __init__(self, message=None):
-        if not message:
-            message = u'Sample Code Required if other data has been entered'
-        self.message = message
-
-    def __call__(self, form, field):
-    	if (not field.data and (form.t1.data or form.s1.data or form.t2.data or form.s2.data)):
-            raise ValidationError(self.message)
-
-class SampleForm(WtfForm):
-    sampleCode = StringField('Sample Code', validators=[RequiredIfSampleDataEntered()])
-    t1 = DecimalField('T1', validators=[RequiredIfSampleCode()], places=1)
-    s1 = DecimalField('S1', validators=[RequiredIfSampleCode()], places=1)
-    t2 = DecimalField('T2', validators=[RequiredIfSampleCode()], places=1)
-    s2 = DecimalField('S2', validators=[RequiredIfSampleCode()], places=1)
 
 class BatchForm(WtfForm):
     batchCode = StringField('Batch Code', validators=[DataRequired(), Length(max=20)])
@@ -34,10 +9,6 @@ class BatchForm(WtfForm):
     pcrMachine = StringField('PCR Machine', validators=[DataRequired(), Length(max=20)])
     temperature = DecimalField('Temperature', validators=[DataRequired()], places=1)
     datetime = DateTimeField('Date and Time', validators=[DataRequired()], format='%d/%m/%Y %H:%M')
-
-class BatchAndSampleForm(Form):
-    batch = FormField(BatchForm)
-    samples = FieldList(FormField(SampleForm), min_entries=48)
 
 class BatchEditForm(Form):
     id = HiddenField('id')
--- app/views/batch.py
+++ app/views/batch.py
@@ -4,28 +4,11 @@ from flask.ext.login import login_required
 from app import db, telomere
 from app.services.batch import BatchService
 from app.services.sample import SampleService
-from app.forms.batch import BatchAndSampleForm, BatchDelete, BatchEditForm
+from app.forms.batch import BatchDelete, BatchEditForm
 from app.model.batch import Batch
 from app.model.sample import Sample
 from app.model.measurement import Measurement
 from flask_login import current_user
-
-@telomere.route("/batch/entry", methods=['GET', 'POST'])
-@login_required
-def batch_entry():
-    form = BatchAndSampleForm(batch = {'datetime': datetime.datetime.now()})
-
-    if form.validate_on_submit():
-        batchService = BatchService()
-        batch = batchService.SaveAndReturn(form.batch)
-
-        if (batch):
-            _saveSampleMeasurements(form, batch)
-
-            db.session.commit()
-            return redirect(url_for('batch_index'))
-
-    return render_template('batch/batchEntry.html', form=form)
 
 @telomere.route("/batch/edit/<int:id>", methods=['GET','POST'])
 @login_required
@@ -53,26 +36,6 @@ def batch_edit(id):
             return redirect(url_for('batch_index'))
 
     return render_template('batch/batchEdit.html', form=form)
-
-def _saveSampleMeasurements(form, batch):
-    for sm in form.samples.entries:
-        
-        if not sm.sampleCode.data: continue
-
-        sampleId = sm.sampleCode.data
-
-        sampleService = SampleService()
-        sample = sampleService.GetOrCreateSample(sampleId)
-
-        measurement = Measurement(
-            batchId=batch.id,
-            sampleId=sample.id,
-            t1=sm.t1.data,
-            s1=sm.s1.data,
-            t2=sm.t2.data,
-            s2=sm.s2.data,
-            )
-        db.session.add(measurement)
 
 @telomere.route('/batch/')
 @telomere.route("/batch/page:<int:page>")
