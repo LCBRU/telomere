2fb363e5509ac155c3f8035ec92c63eeea2a8849
Richard Bramley <rabramley@gmail.com>
2015-11-30 15:47:14 +0000

    Add users

---
+++ app/db_upgrade/000030.sql
@@ -0,0 +1,19 @@
+INSERT INTO user (username, code)
+VALUES ('md123', '0003')
+;
+
+INSERT INTO user (username, code)
+VALUES ('cs597', '0004')
+;
+
+INSERT INTO user (username, code)
+VALUES ('ss931', '0005')
+;
+
+INSERT INTO user (username, code)
+VALUES ('den4', '0006')
+;
+
+INSERT INTO user (username, code)
+VALUES ('mp483', '0007')
+;
