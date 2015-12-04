32fee35d18c9866755fcd7779781d7bf692a682d
Richard Bramley <rabramley@gmail.com>
2015-12-04 13:50:07 +0000

    Spike logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,21 @@ from flask import Flask, g
 from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 
-logging.basicConfig()
+    #logging.basicConfig()
 
 telomere = Flask(__name__)
 telomere.config.from_object('app.settings')
+
+ADMINS = ['rab63@le.ac.uk']
+if not telomere.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler('127.0.0.1',
+                               'lcbruit@le.ac.uk',
+                               ADMINS, 'Telomere Failed')
+    mail_handler.setLevel(logging.ERROR)
+    telomere.logger.addHandler(mail_handler)
+
 
 db = SQLAlchemy(telomere)
 
@@ -19,6 +30,11 @@ telomere.secret_key = telomere.config['SECRET_KEY']
 def set_date():
     g.year = datetime.datetime.now().year
 
+@telomere.errorhandler(500)
+def internal_error(exception):
+    telomere.logger.error(exception)
+    return render_template('500.html'), 500
+        
 import app.helpers.templateFilters
 
 from app.views import *
