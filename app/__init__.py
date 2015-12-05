b79f0e7df407976072409648c70d6fde18e3690d
Richard Bramley <rabramley@gmail.com>
2015-12-05 10:43:21 +0000

    Un-Refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,21 @@ telomere = Flask(__name__)
 telomere.config.from_object('app.settings')
 telomere.secret_key = telomere.config['SECRET_KEY']
 
-import errorHandling
+if not telomere.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler(telomere.config['SMTP_SERVER'],
+                               telomere.config['APPLICATION_EMAIL_ADDRESSES'],
+                               telomere.config['ADMIN_EMAIL_ADDRESSES'],
+                               telomere.config['ERROR_EMAIL_SUBJECT'])
+    mail_handler.setLevel(logging.ERROR)
+    telomere.logger.addHandler(mail_handler)
+
+@telomere.errorhandler(500)
+@telomere.errorhandler(Exception)
+def internal_error(exception):
+    telomere.logger.error(traceback.format_exc())
+    return render_template('500.html'), 500
 
 db = SQLAlchemy(telomere)
 
