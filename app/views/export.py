5a8a20841fd735ee00a9d3bf47478cfd21b90dbd
Richard Bramley <rabramley@gmail.com>
2015-12-15 09:04:36 +0000

    Export my outstanding errors

--- app/views/export.py
+++ app/views/export.py
@@ -1,10 +1,12 @@
 from flask import g, send_file, render_template
 from flask.ext.login import login_required
 import tempfile, os, csv, datetime
+from flask_login import current_user
 
 from app import telomere, db
 
 from app.model.measurement import Measurement
+from app.model.outstandingError import OutstandingError
 
 @telomere.route('/exports')
 @login_required
@@ -19,21 +21,37 @@ def export_all_measurements():
     try:
         _write_all_measurements_csv(f)
 
-        f.seek(0)
-        response = send_file(f, as_attachment=True, attachment_filename="telomere_all_measurements_%s.csv" % datetime.datetime.now().strftime('%Y%M%d%H%m%S'),
-                             add_etags=False)
-        f.seek(0, os.SEEK_END)
-        size = f.tell()
-        f.seek(0)
-        response.headers.extend({
-            'Content-Length': size,
-            'Cache-Control': 'no-cache'
-        })
-        
-        return response
-    
+        return _send_csv_to_response(f)
+
     finally:
 	    f.close
+
+@telomere.route('/exports/my_errors')
+@login_required
+def export_my_errors():
+    f = tempfile.TemporaryFile()
+
+    try:
+        _write_my_errors_csv(f, current_user.id)
+
+        return _send_csv_to_response(f)
+
+    finally:
+        f.close
+
+def _send_csv_to_response(f):
+    f.seek(0)
+    response = send_file(f, as_attachment=True, attachment_filename="telomere_all_measurements_%s.csv" % datetime.datetime.now().strftime('%Y%M%d%H%m%S'),
+                         add_etags=False)
+    f.seek(0, os.SEEK_END)
+    size = f.tell()
+    f.seek(0)
+    response.headers.extend({
+        'Content-Length': size,
+        'Cache-Control': 'no-cache'
+    })
+    
+    return response
 
 def _write_all_measurements_csv(outputFile):
     COL_BATCH_CODE = 'BatchId'
@@ -136,4 +154,41 @@ def _write_all_measurements_csv(outputFile):
             COL_ERRORLOWT_TO : measurement.errorLowT_to,
             COL_ERRORHIGHCV : measurement.errorHighCv,
             COL_ERROR_INVALIDSAMPLECOUNT : measurement.errorInvalidSampleCount
+            })
+
+def _write_my_errors_csv(outputFile, user_id):
+    COL_SAMPLE_CODE = 'Sample Code'
+    COL_PLATE_NAME = 'Plate Name'
+    COL_WELL = 'Well'
+    COL_CONDITION_DESCRIPTION = 'Condition'
+    COL_DNA_TEST = 'DNA Test'
+    COL_PICO_TEST = 'PICO Test'
+    COL_VOLUME = 'Volume'
+
+    fieldnames = [
+        COL_SAMPLE_CODE,
+        COL_PLATE_NAME,
+        COL_WELL,
+        COL_CONDITION_DESCRIPTION,
+        COL_DNA_TEST,
+        COL_PICO_TEST,
+        COL_VOLUME
+        ]
+
+    output = csv.DictWriter(outputFile, fieldnames=fieldnames)
+
+    output.writer.writerow(output.fieldnames)
+
+    for oe in (OutstandingError
+                .query
+                .join(OutstandingError.batch)
+                .filter_by(operatorUserId=user_id)):
+        output.writerow({
+            COL_SAMPLE_CODE : oe.sample.sampleCode,
+            COL_PLATE_NAME : oe.sample.plateName,
+            COL_WELL : oe.sample.well,
+            COL_CONDITION_DESCRIPTION : oe.sample.conditionDescription,
+            COL_DNA_TEST : oe.sample.dnaTest,
+            COL_PICO_TEST : oe.sample.picoTest,
+            COL_VOLUME : oe.sample.volume
             })
