0f7b8cd8d259e9e7d0eea583f62a38ca146e8020
Richard Bramley <rabramley@gmail.com>
2015-12-22 11:14:19 +0000

    Change counts on home page

--- app/views/home.py
+++ app/views/home.py
@@ -4,13 +4,15 @@ from flask.ext.login import login_required
 from app import telomere
 
 from app.model.batch import Batch
-from app.model.sample import Sample
+from app.model.samplePlate import SamplePlate
 from app.model.measurement import Measurement
+from app.model.manifest import Manifest
 
 @telomere.route('/')
 def index():
+	manifests = Manifest.query.count()
 	batches = Batch.query.count()
-	samples = Sample.query.count()
+	samplePlates = SamplePlate.query.count()
 	measurements = Measurement.query.count()
 
-	return render_template('index.html', batches=batches, samples=samples, measurements=measurements)
+	return render_template('index.html', batches=batches, samplePlates=samplePlates, measurements=measurements, manifests=manifests)
