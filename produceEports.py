d0a7b706aabd31385b13b0936c84eea8f49f0d39
Richard Bramley <rabramley@gmail.com>
2016-06-16 14:51:19 +0100

    Link to cached export

--- produceEports.py
+++ produceEports.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from app.views.export import write_all_measurements_csv
 import tempfile
+import os
 
-f = open("app/static/exports/AllMeasurements.csv", "w")
+f = open("app/static/exports/AllMeasurements_inprogress.csv", "w")
 
 try:
     write_all_measurements_csv(f)
 
 finally:
     f.close
+
+os.rename("app/static/exports/AllMeasurements_inprogress.csv", "app/static/exports/AllMeasurements.csv")
 
