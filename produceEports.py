ddb64a0b7a09203c8367c47d34ac29a82af012c0
Richard Bramley <rabramley@gmail.com>
2016-07-02 08:47:30 +0100

    Use directory for all interaction - duh!

--- produceEports.py
+++ produceEports.py
@@ -3,7 +3,11 @@ from app.views.export import write_all_measurements_csv
 import tempfile
 import os
 
-f = open("{0}/app/static/exports/AllMeasurements_inprogress.csv".format(os.path.dirname(os.path.realpath(__file__))), "w")
+exportDirectory = "{0}/app/static/exports".format(os.path.dirname(os.path.realpath(__file__))
+workingFile = "{0}/AllMeasurements_inprogress.csv".format(exportDirectory)
+finalFile = "{0}/AllMeasurements.csv".format(exportDirectory)
+
+f = open(workingFile, "w")
 
 try:
     write_all_measurements_csv(f)
@@ -11,5 +15,5 @@ try:
 finally:
     f.close
 
-os.rename("app/static/exports/AllMeasurements_inprogress.csv", "app/static/exports/AllMeasurements.csv")
+os.rename(workingFile, finalFile)
 
