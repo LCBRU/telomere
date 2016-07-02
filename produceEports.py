9c2cdf5113acbb50690a3ab3466c307c0e04ab85
Richard Bramley <rabramley@gmail.com>
2016-07-02 08:05:52 +0100

    Python 2.6 on server

--- produceEports.py
+++ produceEports.py
@@ -3,7 +3,7 @@ from app.views.export import write_all_measurements_csv
 import tempfile
 import os
 
-f = open("{}/app/static/exports/AllMeasurements_inprogress.csv".format(os.path.dirname(os.path.realpath(__file__))), "w")
+f = open("{0}/app/static/exports/AllMeasurements_inprogress.csv".format(os.path.dirname(os.path.realpath(__file__))), "w")
 
 try:
     write_all_measurements_csv(f)
