80a12a109476d73b6302a62969e573cafba7d078
Richard Bramley <rabramley@gmail.com>
2016-07-02 08:48:38 +0100

    Syntax error

--- produceEports.py
+++ produceEports.py
@@ -3,7 +3,7 @@ from app.views.export import write_all_measurements_csv
 import tempfile
 import os
 
-exportDirectory = "{0}/app/static/exports".format(os.path.dirname(os.path.realpath(__file__))
+exportDirectory = "{0}/app/static/exports".format(os.path.dirname(os.path.realpath(__file__)))
 workingFile = "{0}/AllMeasurements_inprogress.csv".format(exportDirectory)
 finalFile = "{0}/AllMeasurements.csv".format(exportDirectory)
 
