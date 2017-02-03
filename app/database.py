42d426ac619668e5839897908c48302b0f91fa70
Richard Bramley <rabramley@gmail.com>
2017-02-03 10:50:27 +0000

    Reformat after linting

--- app/database.py
+++ app/database.py
@@ -1,30 +1,38 @@
-import os, datetime
+import os
+import datetime
 import sys
 from app import db, telomere
-from flask import g
-from contextlib import closing
 import traceback
 
-dbUpgradeDir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db_upgrade')
+dbUpgradeDir = os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), 'db_upgrade')
 
 print dbUpgradeDir
 
-def init_db() :
+
+def init_db():
     print 'initialising DB'
-    db.engine.execute("CREATE TABLE IF NOT EXISTS db_version (id INT AUTO_INCREMENT, version INT, appliedDate DATETIME, PRIMARY KEY(id));")
-    currentVersion = db.engine.execute("SELECT MAX(version) maxVersion FROM db_version").fetchall()[0][0] or 0
+    db.engine.execute(
+        '''CREATE TABLE IF NOT EXISTS db_version (
+            id INT AUTO_INCREMENT,
+            version INT,
+            appliedDate DATETIME,
+            PRIMARY KEY(id));''')
+
+    currentVersion = db.engine.execute(
+        "SELECT MAX(version) maxVersion FROM db_version").fetchall()[0][0] or 0
 
     print 'Upgrading DB from version %d' % currentVersion
 
     print 'Upgrade directory is ' + dbUpgradeDir
 
     upgradeScripts = [f for f in os.listdir(dbUpgradeDir)
-                        if f.split('.')[0].isdigit() 
-                            and f.split('.')[1] == 'sql' 
-                            and os.path.isfile(os.path.join(dbUpgradeDir, f))
-                            and int(f.split('.')[0]) > currentVersion]
-    
-    upgradeScripts.sort(key = lambda s: int(s.split('.')[0]))
+                      if f.split('.')[0].isdigit() and
+                      f.split('.')[1] == 'sql' and
+                      os.path.isfile(os.path.join(dbUpgradeDir, f)) and
+                      int(f.split('.')[0]) > currentVersion]
+
+    upgradeScripts.sort(key=lambda s: int(s.split('.')[0]))
 
     for f in upgradeScripts:
         print 'Running DB script %s' % f
@@ -32,13 +40,16 @@ def init_db() :
         with open(os.path.join(dbUpgradeDir, f)) as s:
             try:
                 curUpdate = db.engine.raw_connection().cursor()
-                curUpdate.execute("START TRANSACTION;\n" + s.read() + "\nCOMMIT; ")
+                curUpdate.execute(
+                    "START TRANSACTION;\n" + s.read() + "\nCOMMIT; ")
                 curUpdate.close()
 
-                db.engine.execute('INSERT INTO db_version (version, appliedDate) VALUES (%s, %s)', [int(f.split('.')[0]), datetime.datetime.now()])
+                db.engine.execute(
+                    '''INSERT INTO db_version (version, appliedDate)
+                        VALUES (%s, %s)''',
+                    [int(f.split('.')[0]), datetime.datetime.now()])
             except:
                 telomere.logger.error(traceback.format_exc())
                 db.engine.raw_connection().cursor().execute("ROLLBACK;")
                 print "Unexpected Error: ", sys.exc_info()[0]
                 raise
-
--- app/services/batch.py
+++ app/services/batch.py
@@ -1,31 +1,28 @@
-import datetime
-from flask import flash
 from flask_login import current_user
 from app import db
 from app.model.batch import Batch
 from app.model.outstandingError import OutstandingError
 import numpy
-from sets import Set
-from decimal import *
+
 
 class BatchService():
 
     def SaveAndReturn(self, batchForm):
         batch = Batch(
-            robot = batchForm.robot.data,
-            temperature = batchForm.temperature.data,
-            datetime = batchForm.datetime.data,
-            userId = current_user.id,
-            plateName = batchForm.plateName.data,
-            halfPlate = batchForm.halfPlate.data,
-            humidity = batchForm.humidity.data,
-            primerBatch = batchForm.primerBatch.data,
-            enzymeBatch = batchForm.enzymeBatch.data,
-            rotorGene = batchForm.rotorGene.data,
-            operatorUserId = batchForm.operatorUserId.data,
-            batchFailureReason = batchForm.batchFailureReason.data,
-            processType = batchForm.processType.data
-            )
+            robot=batchForm.robot.data,
+            temperature=batchForm.temperature.data,
+            datetime=batchForm.datetime.data,
+            userId=current_user.id,
+            plateName=batchForm.plateName.data,
+            halfPlate=batchForm.halfPlate.data,
+            humidity=batchForm.humidity.data,
+            primerBatch=batchForm.primerBatch.data,
+            enzymeBatch=batchForm.enzymeBatch.data,
+            rotorGene=batchForm.rotorGene.data,
+            operatorUserId=batchForm.operatorUserId.data,
+            batchFailureReason=batchForm.batchFailureReason.data,
+            processType=batchForm.processType.data
+        )
 
         db.session.add(batch)
         db.session.flush()
@@ -33,19 +30,19 @@ class BatchService():
         return batch
 
     def IsBatchDuplicate(self, batch):
-        return (Batch
-            .query
-            .filter_by(plateName=batch.plateName)
-            .filter_by(halfPlate=batch.halfPlate)
-            .filter(Batch.id != batch.id)
-            .count()
-            ) > 0
+        return (Batch.query
+                .filter_by(plateName=batch.plateName)
+                .filter_by(halfPlate=batch.halfPlate)
+                .filter(Batch.id != batch.id)
+                .count()
+                ) > 0
 
     def SetCoefficientsOfVariation(self, batch):
         for m in batch.measurements:
-            measurements = batch.get_measurements_for_sample_code(m.sample.sampleCode)
-            
-            tsValues = [ x.ts for x in measurements if x.ts is not None]
+            measurements = batch.get_measurements_for_sample_code(
+                m.sample.sampleCode)
+
+            tsValues = [x.ts for x in measurements if x.ts is not None]
 
             m.errorInvalidSampleCount = False
             m.errorHighCv = False
@@ -56,7 +53,7 @@ class BatchService():
 
             mean = numpy.mean(tsValues)
             std = numpy.std(tsValues, ddof=1)
-            m.coefficientOfVariation = round( std / mean * 100, 6)
+            m.coefficientOfVariation = round(std / mean * 100, 6)
             m.errorHighCv = m.coefficientOfVariation > 10
 
     def GetValidationErrors(self, batch):
@@ -65,9 +62,9 @@ class BatchService():
         for m in batch.measurements:
             for ed in m.GetValidationErrors():
                 result.append(OutstandingError(
-                    description = ed,
-                    batchId = batch.id,
-                    sampleId = m.sample.id
-                    ))
+                    description=ed,
+                    batchId=batch.id,
+                    sampleId=m.sample.id
+                ))
 
         return result
